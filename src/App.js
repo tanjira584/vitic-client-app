67c8e814d2cef48eaffbfed5913361e73ac58525
Tanjir Ahmed <tanjira584@gmail.com>
2022-09-14 08:35:52 +0600

    Environment Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="">
+            <h2 className="text-center text-danger">Hello World</h2>
+            <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+            <ToastContainer></ToastContainer>
+        </div>
+    );
 }
 
 export default App;
